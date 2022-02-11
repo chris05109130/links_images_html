59c6c1f29091bcd4e4fd0604e5cfb27b5cd2f5e0
chris05109130 <92364093+chris05109130@users.noreply.github.com>
2022-02-11 02:42:35 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Images and Anchors</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+  <section id="mainSection">
+    <h1 class="explain">Images</h1>
+    <h1 class="explain">Which are ways to put images on your web pages</h1>
+
+    <p class="example">Example of an image would be</p>
+    <p class="example">
+      <img src="comp.jpeg" alt="Computers">
+    </p>
+    <h1 class="explain">Anchors</h1>
+    <h1 class="explain">Which are links to add to a webpage or go to a webpage</h1>
+
+    <p class="example">Example of a link would be</p>
+    <p class="example"><a href="http://www.google.com">Google Link</a></p>
+  </section>
+
+</body>
+</html>
